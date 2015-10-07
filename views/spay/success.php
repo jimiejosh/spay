2f6e749dd0e5ee73454c2f594af4d0d1facfe474
jimie josh <joshjimie@gmail.com>
2015-10-07 07:45:48 +0100

    Create success.php

---
+++ views/spay/success.php
@@ -0,0 +1,14 @@
+<?php
+
+<h2>Transaction Successfully</h2>
+<p>You will be redirected in <span id="counter">10</span> second(s).</p>
+<script type="text/javascript">
+function countdown() {
+    var i = document.getElementById('counter');
+    if (parseInt(i.innerHTML)<=0) {
+        location.href = '<?php echo base_url(); ?>';
+    }
+    i.innerHTML = parseInt(i.innerHTML)-1;
+}
+setInterval(function(){ countdown(); },1000);
+</script>
